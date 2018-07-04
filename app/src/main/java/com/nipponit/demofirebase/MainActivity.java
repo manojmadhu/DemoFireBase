719d6912ae383bbab6ee1e486caaef5d4dcbd633
manojmadhu <kammadhuranga@hotmail.com>
2018-07-04 16:50:42 +0530

    Initial commit

---
+++ app/src/main/java/com/nipponit/demofirebase/MainActivity.java
@@ -0,0 +1,57 @@
+package com.nipponit.demofirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private FirebaseDatabase contactdb;
+    private DatabaseReference RootcontactRefernce;
+    private DatabaseReference ChildcontactReference;
+    TextView lblname;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        contactdb = FirebaseDatabase.getInstance();
+        RootcontactRefernce = contactdb.getReference("contacts");
+        RootcontactRefernce.child("name").setValue("");
+        ChildcontactReference = RootcontactRefernce.child("name");
+
+        lblname = (TextView)findViewById(R.id.lblname);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ChildcontactReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.getValue(String.class);
+                lblname.setText(name.toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
